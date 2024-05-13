4da05d659199214b6a52484b600fc70c2e68df74
Duong <daokimduong322@gmail.com>
2024-05-13 10:12:41 +0700

    home page without searching restaurant feature

--- src/main/java/com/duongdk/edu/Hiruez/controller/CustomerController.java
+++ src/main/java/com/duongdk/edu/Hiruez/controller/CustomerController.java
@@ -13,6 +13,9 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -226,6 +229,23 @@ public class CustomerController {
 		newInvoice.setCreatedTime(LocalDateTime.now());
 		invoiceRepository.save(newInvoice);
 		return "redirect:/customer/store/" + storeId + "/table/" + tableId + "/invoice/detail" ;
+	}
+	
+	
+	// -----------------------------------
+	// add new template
+	// -----------------------------------
+	
+	@GetMapping("/")
+	public String getHomePage2(Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		User curUser = userRepository.findByUsername(authentication.getName());
+		model.addAttribute("currentUser", curUser);
+
+		// get list of only 5 stores with highest by rating decreasing
+		List<Store> stores = storeRepository.findAllByOrderByRatingDesc();
+		model.addAttribute("stores", stores);
+		return "index";
 	}
 	
 }
